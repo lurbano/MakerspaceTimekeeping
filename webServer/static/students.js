e8d96ec8aa4423971319866d40fbf2bc54b9ad91
lurbano <lurbano@earthsciweb.org>
2022-02-09 08:57:50 -0600

    updated student list

--- webServer/static/students.js
+++ webServer/static/students.js
@@ -3,170 +3,378 @@ var roll = [
     name: 'Doc',
     id: 101,
     grade: 1999,
-    classTime: [{"day":"Dr. Urbano", "time":"11:30"}]},
+    classTime: [{"day":"Dr. Urbano", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Ella',
     id: 1,
     grade: 2023,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Holden',
     id: 2,
     grade: 2024,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Sofia',
     id: 3,
     grade: 2024,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Kate',
     id: 4,
     grade: 2023,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Andrew H.',
     id: 5,
     grade: 2022,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Maddy',
     id: 6,
     grade: 2023,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Lizzie',
     id: 7,
     grade: 2023,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Starley',
     id: 8,
     grade: 2023,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Coen',
     id: 9,
     grade: 2023,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Sam',
     id: 10,
     grade: 2023,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Alex',
     id: 11,
     grade: 2023,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Julian',
     id: 12,
     grade: 2024,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Blas',
     id: 13,
     grade: 2023,
-    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}]},
+    classTime: [{"day":"Monday", "time":"11:30"},{"day":"Thursday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'George',
     id: 14,
     grade: 2021,
-    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}]},
+    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Darian',
     id: 15,
     grade: 2023,
-    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}]},
+    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Kristina',
     id: 16,
     grade: 2021,
-    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}]},
+    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Brooke',
     id: 17,
     grade: 2022,
-    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}]},
+    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Adam',
     id: 18,
     grade: 2022,
-    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}]},
+    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Jordan',
     id: 19,
     grade: 2022,
-    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}]},
+    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'John',
     id: 20,
     grade: 2022,
-    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}]},
+    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Ryan',
     id: 21,
     grade: 2024,
-    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}]},
+    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Dobby',
     id: 22,
     grade: 2023,
-    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}]},
+    classTime: [{"day":"Tuesday", "time":"11:30"},{"day":"Friday", "time":"11:30"}],
+    frontpage: true
+  },
   {
     name: 'Ben',
     id: 23,
     grade: 2025,
-    classTime: [{"day":"Interim", "time":"Creativity"}]},
+    classTime: [{"day":"Interim", "time":"Creativity"}],
+    frontpage: true
+  },
   {
     name: 'Darian',
     id: 24,
     grade: 2023,
-    classTime: [{"day":"Interim", "time":"Creativity"}]},
+    classTime: [{"day":"Interim", "time":"Creativity"}],
+    frontpage: true
+  },
   {
     name: 'Katarina',
     id: 25,
     grade: 2022,
-    classTime: [{"day":"Interim", "time":"Creativity"}]},
+    classTime: [{"day":"Interim", "time":"Creativity"}],
+    frontpage: true
+  },
   {
     name: 'Hali',
     id: 26,
     grade: 2023,
-    classTime: [{"day":"Interim", "time":"Creativity"}]},
+    classTime: [{"day":"Interim", "time":"Creativity"}],
+    frontpage: true
+  },
   {
     name: 'Abri',
     id: 27,
     grade: 2026,
-    classTime: [{"day":"Interim", "time":"Creativity"}]},
+    classTime: [{"day":"Interim", "time":"Creativity"}],
+    frontpage: true
+  },
   {
     name: 'Liam',
     id: 28,
     grade: 2026,
-    classTime: [{"day":"Interim", "time":"Creativity"}]},
+    classTime: [{"day":"Interim", "time":"Creativity"}],
+    frontpage: true
+  },
   {
     name: 'Miles',
     id: 29,
     grade: 2026,
-    classTime: [{"day":"Interim", "time":"Creativity"}]},
+    classTime: [{"day":"Interim", "time":"Creativity"}],
+    frontpage: true
+  },
   {
     name: 'Ava',
     id: 30,
     grade: 2024,
-    classTime: []},
+    classTime: [],
+    frontpage: true
+  },
   {
     name: 'Bryce',
     id: 31,
     grade: 2022,
-    classTime: []},
+    classTime: [],
+    frontpage: true
+  },
   {
     name: 'Harley',
-    id: 30,
+    id: 32,
     grade: 2023,
-    classTime: []},
-    {
-      name: 'Mr. Mitchell',
-      id: 101,
-      grade: 1999,
-      classTime: [{"day":"Faculty", "time":"HS"}]},
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'James',
+    id: 33,
+    grade: 2021,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Jack F.',
+    id: 34,
+    grade: 2021,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Rachel',
+    id: 35,
+    grade: 2021,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Isaac',
+    id: 36,
+    grade: 2021,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Caleb',
+    id: 37,
+    grade: 2021,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Maya',
+    id: 38,
+    grade: 2022,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Darian',
+    id: 39,
+    grade: 2022,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Joy',
+    id: 40,
+    grade: 2022,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Luke',
+    id: 41,
+    grade: 2022,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Ana',
+    id: 42,
+    grade: 2022,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Andrew K.',
+    id: 43,
+    grade: 2023,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Ruthie',
+    id: 44,
+    grade: 2023,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Charlotte',
+    id: 45,
+    grade: 2023,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Mary L.',
+    id: 46,
+    grade: 2024,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Sally',
+    id: 47,
+    grade: 2025,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Spencer',
+    id: 48,
+    grade: 2025,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Will',
+    id: 49,
+    grade: 2025,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Maybelle',
+    id: 50,
+    grade: 2025,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Lily',
+    id: 51,
+    grade: 2025,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Damian',
+    id: 52,
+    grade: 2026,
+    classTime: [],
+    frontpage: true
+  },
+  {
+    name: 'Mr. Mitchell',
+    id: 101,
+    grade: 1999,
+    classTime: [{"day":"Faculty", "time":"HS"}],
+    frontpage: true
+  },
 ];
